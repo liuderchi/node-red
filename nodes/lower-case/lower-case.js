2330ffd8a7afef5377d7ae4c22e0f910f7f52685
Te-Chi Liu <liuderchi@gmail.com>
2018-08-10 08:49:09 +0800

    refactor: PureNode.js

--- nodes/lower-case/lower-case.js
+++ nodes/lower-case/lower-case.js
@@ -1,16 +1,6 @@
-const lowerCase = ({ payload }) =>
+const PureNode = require('./utils/PureNode');
+
+const lowerCase = payload =>
   typeof payload === 'string' ? payload.toLowerCase() : payload;
 
-module.exports = RED => {
-  function LowerCaseNode(config) {
-    RED.nodes.createNode(this, config);
-    const node = this;
-    node.on('input', msg => {
-      node.send({
-        ...msg,
-        payload: lowerCase(msg),
-      });
-    });
-  }
-  RED.nodes.registerType('lower-case', LowerCaseNode);
-};
+module.exports = PureNode('lower-case', lowerCase);
---
+++ nodes/lower-case/utils/PureNode.js
@@ -0,0 +1,12 @@
+module.exports = (name, fcn) => RED => {
+  RED.nodes.registerType(name, function(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    node.on('input', msg => {
+      node.send({
+        ...msg,
+        payload: fcn(msg.payload),
+      });
+    });
+  });
+};
