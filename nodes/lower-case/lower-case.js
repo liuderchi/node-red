413f9a85b5523bf05ea848f1100575c02199ae9b
liuderchi <liuderchi@gmail.com>
2018-08-15 18:02:57 +0800

    WIP: poc of ws client with auth
    - TODO: rename names and meta `lower-case` -> `ws-auth`
    - TODO: reveal info for node config

--- nodes/lower-case/lower-case.js
+++ nodes/lower-case/lower-case.js
@@ -1,14 +1,35 @@
-const lowerCase = ({ payload }) =>
-  typeof payload === 'string' ? payload.toLowerCase() : payload;
+const openSocket = require('socket.io-client');
+
+const USER_COUNT = 'user count';
+const WS_API = 'http://localhost:3000';
 
 module.exports = RED => {
   function LowerCaseNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
+    let socket;
     node.on('input', msg => {
-      node.send({
-        ...msg,
-        payload: lowerCase(msg),
+      socket = openSocket(WS_API);
+
+      socket.on('connect', function() {
+        socket.emit('authentication', {
+          username: msg.payload, // e.g. 'Derek'
+          password: 'secret',
+          // TODO more input fields of node-red node
+        });
+        socket.on('authenticated', function(res) {
+          console.warn(`authenticated with result: ${res}`);
+          node.send({
+            ...msg,
+            payload: 'Auth successful',
+          });
+          // add ws use logics
+          socket.on(USER_COUNT, ({ numUsers }) => {
+            node.send({
+              payload: `[USER_COUNT] ${numUsers}`,
+            });
+          });
+        });
       });
     });
   }
